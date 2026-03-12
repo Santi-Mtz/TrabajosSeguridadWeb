c257e8b4d4bfb5bda07256aaeea5382b5d80d485
Santi-Mtz <santimtzh@gmail.com>
2026-03-12 17:14:43 -0600

    Practica_2 Seguridad WEB

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+    provideRouter(routes),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+          darkModeSelector: false
+        }
       }
-    }),
-    provideRouter(routes)
+    })
   ]
 };
--- app.ts
+++ app.ts
@@ -1,14 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Practica_1');
-}
-
+export class App {}
