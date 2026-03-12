9ccd7ee60405d473a8fbff60e6b44b64cce0265b
Santi-Mtz <santimtzh@gmail.com>
2026-03-12 17:36:08 -0600

    Practica_6 Seguridad WEB

--- app.ts
+++ app.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  constructor(public router: Router) {}
+
+  projectName = 'Practica 2';
+  appVersion = '4.0';
+  menuOpen = true;
+
+  get showSidebar(): boolean {
+    const hiddenRoutes = ['/landing', '/login', '/register'];
+    return !hiddenRoutes.some((route) => this.router.url.startsWith(route));
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  navigateTo(path: '/dashboard' | '/group' | '/user'): void {
+    void this.router.navigate([path]);
+  }
+}
